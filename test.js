dfb867d61fefa06635c458b4de36ebafa010b750
Tschawi <89629023+Tschawi@users.noreply.github.com>
2022-09-06 15:57:59 +0200

    Game simulation

--- test.js
+++ test.js
@@ -33,8 +33,31 @@ class Team {
 
 var a = [];
 var tm1 = new Team("FC Barcelona", "LaLiga");
-var pl1 = new Player("Pedri", "FC Barcelona", 84);
-var pl2 = new Player("Lewandowski", "FC Barcelona", 91);
-tm1.addPlayer(pl1);
-tm1.addPlayer(pl2);
-console.log(tm1.clacRating());
+var tm2 = new Team("Real Madrid", "LaLiga");
+var pl11 = new Player("Pedri", "FC Barcelona", 84);
+tm1.addPlayer(pl11);
+var pl12 = new Player("Lewandowski", "FC Barcelona", 91);
+tm1.addPlayer(pl12);
+var pl21 = new Player("Vinicius jr", "Real Madrid", 86);
+tm2.addPlayer(pl21);
+var pl22 = new Player("Benzema", "Real Madrid", 91);
+tm2.addPlayer(pl22);
+gamesim(tm1, tm2);
+
+function gamesim(Home, Away){
+  let goalshome = 0;
+  let goalsaway = 0;
+  for(let minutes = 0; minutes < 90; minutes++){
+    //Laliga Durchschnittlich 2.5 Goals pro Match
+    //jede Minute 0.0277777% Chanche uf Goal
+    if(Math.floor(Math.random() * 10000) < 277){
+      if(Math.floor(Math.random() * Home.clacRating()) > Math.floor(Math.random() * Away.clacRating())){
+        goalshome++;
+      } else{
+        goalsaway++;
+      }
+    }
+  }
+  document.getElementById('score').innerHTML = goalshome + ":" + goalsaway;
+}
+
